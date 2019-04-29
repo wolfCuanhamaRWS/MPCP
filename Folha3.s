3187449d56f1855c664e6ac78bc4921386fb6fb8
Juliane Marubayashi <33182421+Jumaruba@users.noreply.github.com>
2019-04-29 15:17:49 +0100

    Update Folha3.s

--- Folha3.s
+++ Folha3.s
@@ -3,59 +3,85 @@
 # b) MEDIA – calcula a média dos elementos de um vetor utilizando a sub-rotina SOMA .
 # c) MAXMED – calcula a média dos elementos de dois vetores e retorna a maior das duas médias.
 
+# respecting the sinxtax of registers
+
+.global max_word;
+.type max_word, %function
+
+.data
 
 
-# x0 = vector 1
-# x1 = dim1
-# x2 = vector 2
-# x3 = dim2
+.text
+
+	#x0 = vector 1
+	#x1 = tamanho da sequencia 1
+	#x2 = vector 2
+	#x3 = tamanho da sequencia 2
+
+
+	#x20 = tamanho da sequencia 1
+	#x21 = tamanho da sequencia 2
+	#x22 = media do vector 1
+
+
 
 max_word:
-			STP x29, x30, [SP, #-16]!
-			BL MAXMED
-			MOV x0, x7
-			LDP x29, x30, [SP], #16
-			RET
 
-# LETTER A
+	#x10 = valor atual
+	#x11 = soma
+	#altera registro x0 e x1 é preservado
 
-# x0 = vector (immutable) 
-# x1 = dim (immutable) 
-# x9 = dim
-# x10 = actual value
-# x11 = sum
-# x7 = result
+MAXMED: 	stp x29, x30, [sp, #-16]!
+		mov x29, sp
 
-SOMA: 			MOV x9, x1
-			MOV x11, #0
-CICLO:			CBZ x9, END
-			LDR w10, [x0]
-			ADD x11, x11, x10
-			ADD x0, x0, #4
-			SUB x9, x9, #1
-			B CICLO
-END: 			MOV x7, x11
-			RET
+		mov x20, x1
+		mov x21, x3
+		bl MEDIA
+		mov x22, x0
+
+		mov x0, x2
+		mov x1, x3
+		bl MEDIA
+		mov x23, x0
+
+		CMP x22, x23
+		CSEL x0, x22, x23, GT
+
+		mov sp, x29
+		ldp x29, x30, [sp], #16
+		ret
 
 
-# LETTER B
-MEDIA: 			STP x29, x30, [SP, #-16]!
-			BL SOMA
-			UDIV x7, x7, x1
-			LDP x29, x30, [SP], #16
-			RET
-	
+MEDIA: 		stp x29, x30, [sp, #-16]!
+		mov x29, sp
 
-		
-#LETTER C
-MAXMED: 		STP x29, x30, [SP, #-16]!
-			BL MEDIA
-			MOV x6, x7
-			MOV x0, x2
-			MOV x1, x3
-			BL MEDIA
-			CMP x6, x7
-			CSEL x7, x7, x6, PL
-			LDP x29, x30, [SP], #16
-			RET
+CICLO1: 	BL SOMA
+		sdiv x0, x0, x1
+
+		mov sp, x29
+		ldp x29, x30, [sp], #16
+		RET
+
+
+
+SOMA: 		stp x29, x30, [sp, #-16]!
+		mov x29, sp
+		str x1, [sp, #-16]!
+		mov x11, #0
+
+CICLO: 		cbz x1, END
+		ldr x10, [x0]
+		add x11, x11, x10
+		add x0, x0, #8
+		sub x1, x1, #1
+		b CICLO
+
+END: 		ldr x1, [sp], #16
+		mov sp, x29
+		ldp x29, x30, [sp], #16
+		mov x0, x11
+		RET
+
+
+
 
