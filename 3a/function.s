b4aae9b945a8d60425dde6baf30a4d0dd7c9052e
Juliane Marubayashi <33182421+Jumaruba@users.noreply.github.com>
2019-04-29 12:55:39 +0100

    Update function.s

--- 3a/function.s
+++ 3a/function.s
@@ -9,14 +9,14 @@
 
 max_word:
 			mov w2, 0
-inicio: 	ldrb w1, [x0]
+inicio: 		ldrb w1, [x0]
 			cmp w1, '0'
 			beq fim
 			add w0, w0, 1
 			add w2, w2, 1
 			b inicio
 
-fim: 		mov w0, w2
+fim: 			mov w0, w2
 			ret
 
 
