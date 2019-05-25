2b9fd69f93564a7a75c83029aed4166baf5630c4
Juliane Marubayashi <33182421+Jumaruba@users.noreply.github.com>
2019-05-25 14:56:25 +0100

    Update SIMD_sol.s

--- SIMD_sol.s
+++ SIMD_sol.s
@@ -8,7 +8,8 @@
 .global somaV
 .type somaV, "function" 
 
-somaV: 
+somaV:  
+        stp x29,x30,[sp, #-16]!
         lsr x3, x3, 2  //estamos fazendo 4 operações em um ciclo 
 ciclo:  
         cbz x3, end
@@ -20,4 +21,29 @@ ciclo:
         str q2, [x2], #16 //armazenando o resultado 
         sub x3, x3, #1  //atualizando o contador
         b ciclo
+        
+end:
+        ldp x29,x30,[sp],#16
         ret 
+
+#------------------------------------------------------------------------------------------------------------------------
+
+#b) void altV(float *P, int n, float k) - x0, x1, s0
+
+.text
+.global altV
+.type altV, "function" 
+
+altV:   
+	stp x29,x30,[sp,#-16]!
+        lsr x1, x1, 2
+ciclo:
+        cbz x1, end
+        ldr q1, [x0]
+        fmul v1.4s, v1.4s, v0.s[0] //v0.s0 é o valor de k, já que os registos S e V são o mesmo
+        str q1, [x0], #16
+        sub x1, x1, #1
+        b ciclo
+
+end:    ldp x29,x30,[sp],#16
+        ret
